811c23187aec36de2b45fcc3d54bce84c3feb5cf
Шандалий Дмитрий <tim_fnk@mail.ru>
2024-05-03 16:05:08 +0800

    post review improvemnet added

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,45 +10,40 @@ import UIKit
 class ViewController:
     UIViewController {
     
-    var counter: Int = 0
+    private var counter: Int = 0
     
-    let date = Date()
-
-    
-    @IBOutlet weak var changeHistory: UITextView!
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak private var changeHistoryTextView: UITextView!
+    @IBOutlet weak private var counterLabel: UILabel!
+    @IBOutlet weak private var minusButton: UIButton!
+    @IBOutlet weak private var plusButton: UIButton!
+    @IBOutlet weak private var resetButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         counterLabel.sizeToFit()
-        changeHistory.isEditable = false
-        changeHistory.isScrollEnabled = true
-        // Do any additional setup after loading the view.
+        changeHistoryTextView.isEditable = false
+        changeHistoryTextView.isScrollEnabled = true
     }
-    
 
-    @IBAction func plusButtonDidTap(_ sender: Any) {
+    @IBAction private func plusButtonDidTap(_ sender: Any) {
         counter += 1
         self.counterLabel.text = "Значение счетчика: " + String(counter)
-        self.changeHistory.text += "[\(date)]: значение изменено на +1"
+        self.changeHistoryTextView.text += "[\(Date())]: значение изменено на +1"
     }
     
-    @IBAction func minusButtonDidTap(_ sender: Any) {
+    @IBAction private func minusButtonDidTap(_ sender: Any) {
         if counter != 0 {
             counter -= 1
-            self.changeHistory.text += "[\(date)]: значение изменено на -1"
+            self.changeHistoryTextView.text += "[\(Date())]: значение изменено на -1"
         } else {
-            self.changeHistory.text += "[\(date)]: попытка уменьшить значение счётчика ниже 0"
+            self.changeHistoryTextView.text += "[\(Date())]: попытка уменьшить значение счётчика ниже 0"
         }
         self.counterLabel.text = "Значение счетчика: " + String(counter)
     }
     
-    @IBAction func resetDidTap(_ sender: Any) {
+    @IBAction private func resetDidTap(_ sender: Any) {
         counter = 0
         self.counterLabel.text = "Значение счетчика: " + String(counter)
-        self.changeHistory.text += "[\(date)]: значение сброшено"
+        self.changeHistoryTextView.text += "[\(Date())]: значение сброшено"
     }
 }
 
